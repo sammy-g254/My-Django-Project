00b0dffdc1a5e1af1bcf6ef53fd52ee31d13c2c7
sammy-g254 <gsam65303@gmail.com>
2025-02-17 10:57:04 +0300

    new 5

--- django_app/urls.py
+++ django_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('about/',views.about,name='about')
+    path('index/',views.index,name='index'),
+    path('myurl/',views.myfunction,name='myurl'),
+    path('about/',views.about,name="about")
 ]
--- django_app/views.py
+++ django_app/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
     return HttpResponse("hello world")
-def about(request):
+def index(request):
     return HttpResponse("About eMobilis")
+def myfunction(request):
+    return HttpResponse('Hello,from myfunction')
+def about(request):
+    template=loader.get_template("about.html")
+    return HttpResponse(template.render())
