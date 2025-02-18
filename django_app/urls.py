a14eb5a8d0ebe9c8df93ebf4e7d68109e024afcc
sammy-g254 <gsam65303@gmail.com>
2025-02-18 10:51:48 +0300

    second commit

--- django_app/urls.py
+++ django_app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('index/',views.index,name='index'),
     path('myurl/',views.myfunction,name='myurl'),
-    path('about/',views.about,name="about")
+    path('about/',views.about,name="about"),
+    path('contact/',views.contact,name="contact"),
+    path('services/',views.services,name="services"),
+    path('contactus/',views.contact_us,name="contactus")
 ]
--- django_app/views.py
+++ django_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,15 @@ def myfunction(request):
 def about(request):
     template=loader.get_template("about.html")
     return HttpResponse(template.render())
+def contact(request):
+    my_template_data={
+        'title':'home',
+        'name':'jane',
+        'message':'this is the homepage',
+        'year':2025
+    }
+    return render(request,'home.html',my_template_data)
+def services(request):
+    return render(request, 'services.html')
+def contactus(request):
+    return render(request, 'contactus.html')
